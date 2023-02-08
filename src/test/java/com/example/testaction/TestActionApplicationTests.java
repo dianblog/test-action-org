27f54ddaf142f04a76bfc37ace8a356567238149
chenbin <3375793023@qq.com>
2023-02-08 11:17:47 +0800

    init

---
+++ src/test/java/com/example/testaction/TestActionApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.testaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+        assert 1 == 1;
+    }
+
+}
